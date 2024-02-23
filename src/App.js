6bc0cce6b7895c7e5650837df6872d4c8663c1ba
seunghyun0522 <cvtcvt007@naver.com>
2024-02-23 14:40:13 +0900

    #7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+    if (toDo === "") return;
+    setToDos((currentArray) => [currentArray, ...toDo]);
+    console.log(toDos);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
+      {toDos}
     </div>
   );
 }
