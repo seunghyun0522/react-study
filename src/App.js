e9b46fdda467d95b8b0e41584bb2628277714b1d
seunghyun0522 <cvtcvt007@naver.com>
2024-02-24 13:22:21 +0900

    #6 Movie app start!

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    //console.log(toDo);
-    if (toDo === "") return;
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    console.log(toDos);
-    setToDo("");
-  };
-
-  const deleteBtn = (event) => {
-    const li = event.target.parentElement;
-    li.remove();
-  };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item} <button onClick={deleteBtn}>X</button>
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
