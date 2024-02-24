85a52325445462ec7333cdc2060d2686a3577859
seunghyun0522 <cvtcvt007@naver.com>
2024-02-24 23:54:14 +0900

    please

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,6 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 function Detail() {
-  const x = useParams();
-  console.log(x);
-  const { id } = useParams();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
